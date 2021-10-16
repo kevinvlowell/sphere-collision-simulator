b16c5fa2b8d675d17c103a9a5588f5f65aca1ff0
SamirFarhat17 <samirfarhat17@gmail.com>
2021-10-16 13:33:00 -0400

    Initial script, take some input

---
+++ spheres.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Samir Farhat Dominguez safarhat@bu.edu
+# Copyright 2021 Fahad Farid fahd@bu.edu
+# Kevin, please add your info!
+
+import sys
+
+# Our main
+def run_sim(params):
+    print(params)
+
+
+# Take input
+for line in sys.stdin:
+    run_sim(line)
