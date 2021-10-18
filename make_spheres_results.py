0c604e4f2346c4b56419a2d49721309ca2ad48bb
Kevin Vogt-Lowell <kjv@bu.edu>
2021-10-18 19:32:24 -0400

    file sync for merge

--- make_spheres_results.py
+++ make_spheres_results.py
@@ -1,6 +1,6 @@
 """This program generates a collection
 of results (saved in text files) for the spheres problem by
-repeatedly running spheres.py with the 
+repeatedly running spheres.py with the
 parameters specified in the list "spheres_tests"
 defined below.
 
@@ -13,7 +13,7 @@ compare your results with mine.
 
 """
 
-myid = "samirf"
+myid = "kjv"
 
 from subprocess import run, PIPE
 import os
@@ -30,7 +30,7 @@ spheres_tests=[
 ('tennisball',10.41,200),
 ('zmover',100,300),
 ('xyzmovers',200,200),
-('three',120,200), 
+('three',120,200),
 ('threeonxaxis',50,400),
 ('diagonal',100,200),
 ('randompool',200,500),
@@ -47,7 +47,6 @@ for basename,radius,N in spheres_tests:
 
     T = run(['python','spheres.py',str(radius),str(N)],input=text,stdout=PIPE,
           universal_newlines=True)
-    
+
     with open(f"examples/{basename}_{radius}_{N}_{myid}.txt",'w') as out:
         out.write(T.stdout)
-    
